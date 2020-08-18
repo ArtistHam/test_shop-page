923e372ee1dfc92b37162e974891056aaa8e7457
ArtistHam <great.artist.ham@gmail.com>
2020-08-18 17:23:01 +0300

    product page layout

--- src/Containers/ProductPage.jsx
+++ src/Containers/ProductPage.jsx
@@ -2,12 +2,59 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import "../App.css";
+import "../Stylesheets/ProductPage.css";
+import tick from "../Images/dropdown-off.svg";
 
 class ProductPage extends Component {
   render() {
     return (
-      <div>ПРОДУКТ</div>
+      <div className="product-page">
+        <div className="item-preview" />
+        <div className="product-information">
+          <button type="button" className="back-btn">
+            <img className="back-symbol" alt="Back" src={tick} />
+            НАЗАД
+          </button>
+          <div className="product-category">РУБАШКИ</div>
+          <div className="product-name">Рубашка на пуговицах</div>
+          <div className="action-wrapper">
+            <div className="size-chooser">
+              Размер
+              <img alt="back" src={tick} />
+            </div>
+            <button type="button" className="add-to-cart-btn">В КОРЗИНУ</button>
+          </div>
+          <div className="product-description-wrapper">
+            <article className="product-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Hic molestias explicabo esse, iusto illum veniam eos sed?
+              Non placeat corrupti soluta dolorem! Dicta incidunt a minima.
+              <br />
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet.
+              <br />
+              <br />
+              <br />
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum velit blanditiis qui?
+            </article>
+            <div className="product-specification">
+              <div className="product-specification-properties">
+                <p>Цвет:</p>
+                <p>Состав:</p>
+                <p>Уход:</p>
+                <p>Артикул:</p>
+              </div>
+              <div className="product-specification-values">
+                <p>Черный</p>
+                <p>Шерсть 100%</p>
+                <p>Сухая чистка</p>
+                <p>HHB73100</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
