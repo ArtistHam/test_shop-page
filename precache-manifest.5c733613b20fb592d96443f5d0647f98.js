2915f3b16a506100b89df9ace2f5185de13aa782
ArtistHam <great.artist.ham@gmail.com>
2020-08-19 18:04:23 +0300

    Updates

---
+++ precache-manifest.5c733613b20fb592d96443f5d0647f98.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a0b306574ee7eff155",
+    "url": "/test_shop-page/static/css/main.da54dc48.chunk.css"
+  },
+  {
+    "revision": "d1a0b306574ee7eff155",
+    "url": "/test_shop-page/static/js/main.ac09058c.chunk.js"
+  },
+  {
+    "revision": "82e6c7c95435896bb02e",
+    "url": "/test_shop-page/static/js/runtime~main.b1168f2c.js"
+  },
+  {
+    "revision": "477f85f34f35791f08dd",
+    "url": "/test_shop-page/static/js/2.83d34f4c.chunk.js"
+  },
+  {
+    "revision": "9ca489c08c74ab74a58021c92f1c6b6d",
+    "url": "/test_shop-page/static/media/dropdown-off.9ca489c0.svg"
+  },
+  {
+    "revision": "204bd4c0196d83e71819421bcb2eef12",
+    "url": "/test_shop-page/static/media/1.204bd4c0.png"
+  },
+  {
+    "revision": "443a696493fc1bb2308f597985f00d13",
+    "url": "/test_shop-page/static/media/2.443a6964.png"
+  },
+  {
+    "revision": "6e9a914b0b0f4d942adef47169bf80a5",
+    "url": "/test_shop-page/static/media/3.6e9a914b.png"
+  },
+  {
+    "revision": "7865d3f3b508db07308074d0d4cac256",
+    "url": "/test_shop-page/static/media/4.7865d3f3.png"
+  },
+  {
+    "revision": "03eba401101eb5270e6eb180f0df8b70",
+    "url": "/test_shop-page/static/media/5.03eba401.png"
+  },
+  {
+    "revision": "ebddea1cce9d1538968212697123267f",
+    "url": "/test_shop-page/static/media/6.ebddea1c.png"
+  },
+  {
+    "revision": "53e58f1a6908f9c1e92528a8dc6a4b11",
+    "url": "/test_shop-page/static/media/cart_inactive.53e58f1a.svg"
+  },
+  {
+    "revision": "797223ce6d582bce329ce828337269ac",
+    "url": "/test_shop-page/static/media/cross.797223ce.svg"
+  },
+  {
+    "revision": "30b1d0f221538c5f371d8434a6203127",
+    "url": "/test_shop-page/index.html"
+  }
+];
